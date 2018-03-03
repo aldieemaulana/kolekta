ef1aac364ae5cfd5f56e650c70662e73532b4717
Aldi Maulana <aldieemaulana@gmail.com>
2018-03-04 04:03:47 +0800

    Integrating laravel pdf for generate report

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Kolekta\Providers\RouteServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -213,6 +214,7 @@ return [
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
